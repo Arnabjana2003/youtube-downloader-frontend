5d86935ea36c268445184f310929750249d56683
Arnab Jana <arnabjana0099@gmail.com>
2024-02-13 16:30:17 +0530

    proxy set

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy:{
-      // "/api": "https://youtube-downloader-pink.vercel.app"
-      "/api": "http://localhost:5000"
+      "/api": "https://youtube-downloader-pink.vercel.app"
+      // "/api": "http://localhost:5000"
     }
   },
   plugins: [react()],
